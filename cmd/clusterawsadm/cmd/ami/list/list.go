39f562b66c5e745a823c996993cbc69dc90641f7
Wendy Ha <wendyha.sut@gmail.com>
2026-05-10 18:02:27 +1000

    feat: Adding Missing AMIs and EOL AMIs list
    Signed-off-by: Wendy Ha <wendyha.sut@gmail.com>

--- cmd/clusterawsadm/cmd/ami/list/list.go
+++ cmd/clusterawsadm/cmd/ami/list/list.go
@@ -59,6 +59,7 @@ func ListAMICmd() *cobra.Command {
 		Args: cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			region, _ := flags.GetRegion(cmd)
+			// region, _ := cmd.Flags().GetString("region")
 
 			printer, err := cmdout.New(outputPrinter, os.Stdout)
 			if err != nil {
